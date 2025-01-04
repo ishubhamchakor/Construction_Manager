12ceee21c96ee3deebced2f05dc73685781585d1
Shubham Chakor <shubhamchakor766@gmail.com>
2025-01-04 12:12:24 +0530

    commit first

--- RestAPI_Project/cm3/cm2/ConstructionManager/src/main/java/com/example/demo/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-
-}

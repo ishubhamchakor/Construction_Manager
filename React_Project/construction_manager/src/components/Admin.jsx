d933d1da70b92cc37869358327b570f0573b6d6f
AbhiAndhale <andhaleabhishek000@gmail.com>
2025-02-08 03:36:49 +0530

    Admin.jsx file NavBar Updated

--- React_Project/construction_manager/src/components/Admin.jsx
+++ React_Project/construction_manager/src/components/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProjectRegistration from './ProjectRegistration'; // Importing the ProjectRegistration component
+import { Navbar } from "./Navbar";
 
 const AdminDashboard = () => {
   const [showProjectForm, setShowProjectForm] = useState(false);
@@ -15,8 +16,10 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div className="m-5">
-      <style>{`
+    <>
+      <Navbar />
+      <div className="m-5">
+        <style>{`
         .welcome-section h2 {
             font-size: 26px;
             font-weight: bold;
@@ -97,53 +100,54 @@ const AdminDashboard = () => {
         }
       `}</style>
 
-      {!showProjectForm && !showReportForm ? (
-        <>
-          <div className="text-center welcome-section mt-5 mb-5">
-            <h2 className="text-primary">Welcome, Admin!</h2>
+        {!showProjectForm && !showReportForm ? (
+          <>
+            <div className="text-center welcome-section mt-5 mb-5">
+              <h2 className="text-primary">Welcome, Admin!</h2>
+            </div>
+
+            <div className="row">
+              <div className="col-md-4">
+                <div className="custom-card">
+                  <div className="card-body">
+                    <h5 className="card-title">Create Users</h5>
+                    <p className="card-text">Easily add new users to the system.</p>
+                    <Link to="/register" className="btn btn-custom">User Registration</Link>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6 mb-4">
+                <div className="custom-card">
+                  <div className="card-body">
+                    <h5 className="card-title">Register New Projects</h5>
+                    <p className="card-text">Submit new projects to be managed.</p>
+                    <button className="btn btn-custom" onClick={handleProjectFormToggle}>Project Registration</button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6 mb-4">
+                <div className="custom-card">
+                  <div className="card-body">
+                    <h5 className="card-title">Generate Reports</h5>
+                    <p className="card-text">Create and view system reports.</p>
+                    <button className="btn btn-custom" onClick={handleReportFormToggle}>Generate Report</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : showProjectForm ? (
+          <ProjectRegistration onClose={handleProjectFormToggle} />
+        ) : (
+          <div className="report-form">
+            <h3>Generate Reports</h3>
+            <button className="btn btn-danger" onClick={handleReportFormToggle}>Close</button>
           </div>
-
-          <div className="row">
-            <div className="col-md-4">
-              <div className="custom-card">
-                <div className="card-body">
-                  <h5 className="card-title">Create Users</h5>
-                  <p className="card-text">Easily add new users to the system.</p>
-                  <Link to="/register" className="btn btn-custom">User Registration</Link>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="custom-card">
-                <div className="card-body">
-                  <h5 className="card-title">Register New Projects</h5>
-                  <p className="card-text">Submit new projects to be managed.</p>
-                  <button className="btn btn-custom" onClick={handleProjectFormToggle}>Project Registration</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="custom-card">
-                <div className="card-body">
-                  <h5 className="card-title">Generate Reports</h5>
-                  <p className="card-text">Create and view system reports.</p>
-                  <button className="btn btn-custom" onClick={handleReportFormToggle}>Generate Report</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : showProjectForm ? (
-        <ProjectRegistration onClose={handleProjectFormToggle} />
-      ) : (
-        <div className="report-form">
-          <h3>Generate Reports</h3>
-          <button className="btn btn-danger" onClick={handleReportFormToggle}>Close</button>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 

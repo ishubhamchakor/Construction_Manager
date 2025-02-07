d3049e473ca92cc2486b6f83fc2945a48fbcbc9e
AbhiAndhale <andhaleabhishek000@gmail.com>
2025-02-07 14:17:24 +0530

    redux - store.js updated

--- React_Project/construction_manager/src/redux/Store.js
+++ React_Project/construction_manager/src/redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from  '../redux/Slice/authSlice'; // Correct the import to match the usage below
+import authReducer from '../redux/Slice/authSlice'; // Correct the import to match the usage below
+import projectReducer from '../redux/Slice/projectReducer';
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // Ensure this matches the imported reducer
+    project: projectReducer,
   },
 });
 

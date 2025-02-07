aa1890dc152c37145975c1c5bdbf70e602281578
Sushen Gajare <gajaresushen22@gmail.com>
2025-02-07 18:26:32 +0530

    Resolved merge conflicts

--- React_Project/construction_manager/src/redux/Store.js
+++ React_Project/construction_manager/src/redux/Store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from  './Slice/authSlice'; // Correct the import to match the usage below
+<<<<<<< Updated upstream
 import projectReducer from './Slice/projectReducer';
+=======
+>>>>>>> Stashed changes
 
 const store = configureStore({
   reducer: {
